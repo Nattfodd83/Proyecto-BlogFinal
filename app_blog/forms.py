e2e25f08d35eb155b671af1e07b348cb331e14f7
Ezequiel <ezequielpena@gmail.com>
2022-09-22 12:24:28 +0200

    App_blog Agregado richtext a forms

--- app_blog/forms.py
+++ app_blog/forms.py
@@ -1,10 +1,12 @@
 from django import forms
+from ckeditor.fields import RichTextField
+
 
 class PostForm(forms.Form):
 
     title = forms.CharField(max_length=40)
     subtitle = forms.CharField(max_length=40)
-    body = forms.CharField(max_length=1000)
+    body = RichTextField()
     author = forms.CharField(max_length=40)
     date = forms.DateField()
     image = forms.ImageField()
