c5724bccf6c88fde729db64948e1bb94a49d42b3
Ezequiel <ezequielpena@gmail.com>
2022-10-02 11:04:25 +0200

    Agregados campos personalizados al Modelo user con modelo perfil y agregado al admin

--- app_accounts/admin.py
+++ app_accounts/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
 from app_accounts.models import *
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
 
 admin.site.register(Avatar)
+
+class Accountinline(admin.StackedInline):
+    model = Perfil
+    can_delete = False
+    verbose_name_plural = "Perfiles"
+
+
+class CustomizedUserAdmin (UserAdmin):
+    inlines = (Accountinline, )
+
+admin.site.unregister (User)
+admin.site.register (User, CustomizedUserAdmin)
--- app_accounts/models.py
+++ app_accounts/models.py
@@ -9,3 +9,12 @@ class Avatar(models.Model):
     
     def __str__(self):
         return f'Imagen de: {self.user}'
+
+
+class Perfil(models.Model):
+    user= models.OneToOneField(User, on_delete=models.CASCADE)
+    red_social=models.URLField()
+    descripcion=models.TextField()
+
+    def __str__(self):
+        return self.user.username
